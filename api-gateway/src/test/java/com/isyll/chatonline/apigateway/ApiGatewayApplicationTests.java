b74ee5f053ae0b5c86d8f47fe5abb5ff806beb1d
Ibrahima Sylla <isyll711@gmail.com>
2023-12-16 23:08:29 +0000

    add api-gateway

---
+++ api-gateway/src/test/java/com/isyll/chatonline/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isyll.chatonline.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
