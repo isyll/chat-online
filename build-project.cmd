36fd1f82de7e13d0d14916d7ed428fb0f6aafdbf
Ibrahima Sylla <isyll711@gmail.com>
2023-12-18 16:51:19 +0000

    temporary fixed build-project.cmd

--- build-project.cmd
+++ build-project.cmd
@@ -1,13 +1,17 @@
 @echo off
 setlocal
 
-set "script_directory=%~dp0"
+set "script_directory=%CD%"
 
 set "apiGatewayFolder=%script_directory%\..\api-gateway"
 set "chatFolder=%script_directory%\..\chat"
+set "userFolder=%script_directory%\..\user"
+set "eurekaFolder=%script_directory%\..\eureka-server"
 
 cd "%apiGatewayFolder%" && mvn clean package -DskipTests
 cd "%chatFolder%" && mvn clean package -DskipTests
+cd "%userFolder%" && mvn clean package -DskipTests
+cd "%eurekaFolder%" && mvn clean package -DskipTests
 cd "%script_directory%" && docker-compose -p chat-online up -d --build
 
 endlocal
