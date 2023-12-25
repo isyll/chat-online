92ad18f133269be672db81abd616cb76c4012fca
Ibrahima Sylla <isyll711@gmail.com>
2023-12-25 04:37:58 +0000

    setting up authentication

---
+++ api-gateway/src/main/java/com/isyll/chatonline/apigateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.isyll.chatonline.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+        return security.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                        .permitAll().anyExchange().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults())).build();
+    }
+}
