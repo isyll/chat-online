17467f653159c19a548c7aafbf77deee31ac3965
Ibrahima Sylla <isyll711@gmail.com>
2023-12-27 18:03:11 +0000

    move ChatList

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Sidenav, { Tab } from './components/Sidenav/Sidenav';
-import ChatList from './components/Sidebar/tabs/ChatList';
+import ChatList from './components/Sidebar/ChatList/ChatList';
 
 function App() {
   const [currentTab, setCurrentTab] = useState(Tab.chats);
