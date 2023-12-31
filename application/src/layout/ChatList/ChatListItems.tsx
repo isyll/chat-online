c959ed78351dd7947c2ee971aae66ab361766771
Ibrahima Sylla <isyll711@gmail.com>
2023-12-31 19:04:08 +0000

    feat: add chat items content loader and fix multiple fetch datas in ChatListItems

--- application/src/layout/ChatList/ChatListItems.tsx
+++ application/src/layout/ChatList/ChatListItems.tsx
@@ -1,27 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getMesssages } from '../../api/chatService';
 import { Message } from '../../types/Message';
 import ChatItem from '../../components/ChatItem/ChatItem';
 import { avatarUrl } from '../../api/fakeData';
+import ChatItemLoader from '../../components/ChatItemLoader/ChatItemLoader';
 
 function ChatListItems() {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  getMesssages().then((res) => {
-    setMessages(res);
-  });
+  useEffect(() => {
+    getMesssages().then((res) => {
+      setTimeout(() => {
+        setMessages(res);
+        setIsLoading(false);
+      }, 2000);
+    });
+  }, []);
+
   return (
     <div className="overflow-y-scroll outline-none flex flex-col flex-1">
       <ul>
-        {messages.map((msg, i) => (
-          <ChatItem
-            key={i}
-            name="Jonh Doe"
-            message={msg.content || ''}
-            avatar={avatarUrl}
-            date={new Date(msg.timestamp)}
-          />
-        ))}
+        {isLoading
+          ? Array.from<number>({ length: 6 }).map(() => (
+              <ChatItemLoader key={Math.random()} />
+            ))
+          : messages.map((msg, i) => (
+              <ChatItem
+                key={i}
+                name="Jonh Doe"
+                message={msg.content || ''}
+                avatar={avatarUrl}
+                date={new Date(msg.timestamp)}
+              />
+            ))}
       </ul>
     </div>
   );
